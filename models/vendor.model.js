73d8caca831e17dfa1e9bdbdd8e12308a2cdbf6a
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2024-08-31 15:23:39 +0530

    vendor model

---
+++ models/vendor.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');  
+
+const vendorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    contactDetails: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    vendorCode: {
+        type: String,
+        required: true
+    },
+    onTimeDeliveryRate: {
+        type: Number,
+        required: true
+    },
+    qualityRatingAvg: {
+        type: Number,
+        required: true
+    },
+    averageResponseTime: {
+        type: Number,
+        required: true
+    },
+    fulfillmentRate: {
+        type: Number,
+        required: true
+    },
+});
+
+const Vendor = mongoose.model('Vendor', vendorSchema);
+module.exports = Vendor;
